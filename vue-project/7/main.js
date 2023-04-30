1279b22b8354ca52fd565637be95038b6acb9679
jxaovito <joaovitoorvieiraborba@gmail.com>
2023-04-29 22:26:03 -0300

    fazendo testes com o inline-template, praticando métodos usando o modal e estudo de dados

--- vue-project/7/main.js
+++ vue-project/7/main.js
@@ -1,14 +1,20 @@
-
+Vue.component('teste-view', {
+    data() {
+        return {
+            numero: 0
+        };
+    }
+});
 
 Vue.component('modal', {
-    template: `<div class="modal is-active">
+    template: `<div class="modal is-active" v-bind:style="{ display: computedDisplay }">
     <div class="modal-background"></div>
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">
         <slot name="header">oh hi</slot>
         </p>
-        <button class="delete" aria-label="close"></button>
+        <button class="delete" aria-label="close" @click="closeModal()"></button>
       </header>
       <section class="modal-card-body">
         <slot name="content">new content here for test!!</slot>
@@ -20,16 +26,30 @@ Vue.component('modal', {
         
       </footer>
     </div>
-  </div>`
+  </div>`,
+  data: function() {
+    return {
+        display: 'flex'
+  }
+   }, 
+   computed: {
+       computedDisplay() {
+         return this.display;
+       }
+     },
+
+   methods: {
+       closeModal(){
+       this.display='none';
+           }
+       }   
 }),
 
 
 new Vue({
 
     el: '#root',
-    data: {
-     
-    },
-
  
-})
+    }
+ 
+)
